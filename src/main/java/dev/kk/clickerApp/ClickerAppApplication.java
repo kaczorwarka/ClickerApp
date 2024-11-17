5ef08b6a118596e5af6fc2a4f643bf0a6022e09d
Kuba Kaczmarski <kuba2001kaczmarski@gmail.com>
2024-11-17 08:38:33 +0100

    Init application

---
+++ src/main/java/dev/kk/clickerApp/ClickerAppApplication.java
@@ -0,0 +1,13 @@
+package dev.kk.clickerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kk/clickerApp/ClickerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kk.clickerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
